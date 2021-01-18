bd2aea5f395c356d072e7d5c2d815a1dae968b7a
Alvaro Silva <alvaro123sps@gmail.com>
2021-01-18 16:57:11 -0300

    Converting class components to functional components, using React Hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -14,31 +14,22 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-	unsubscribeFromAuth = null;
-
-	componentDidMount() {
-		const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+	useEffect(() => {
 		checkUserSession();
-	}
+	}, [checkUserSession]);
 
-	componentWillUnmount() {
-		this.unsubscribeFromAuth();
-	}
-
-	render() {
-		return (
-			<div>
-				<Header />
-				<Switch>
-					<Route exact path='/' component={HomePage} />
-					<Route path='/shop' component={ShopPage} />
-					<Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
-					<Route exact path='/checkout' component={CheckoutPage} />
-				</Switch>
-			</div>
-		);
-	}
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/shop' component={ShopPage} />
+				<Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+				<Route exact path='/checkout' component={CheckoutPage} />
+			</Switch>
+		</div>
+	);
 }
 
 const mapStateToProps = createStructuredSelector({
